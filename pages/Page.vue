621c1f893a1f12b131cacebc301e3c0a9263346a
Fifciuu <filip.jdrasik@gmail.com>
2019-05-12 18:22:29 +0200

    No ACF Fallback on Page/Post and Hajime from CDN

--- pages/Page.vue
+++ pages/Page.vue
@@ -4,37 +4,40 @@
     class="page"
     :key="wpData.id"
   >
-    <div
-      class="page__heading margin__y--sm"
-    >
+    <template v-if="wpData.acf && wpData.acf.sections">
       <div
-        class="page__breadcrumbs"
+        class="page__heading margin__y--sm"
       >
+        <div
+          class="page__breadcrumbs"
+        >
+        </div>
+        <h1
+          class="page__title"
+          :class="{
+            'text-align__left' : wpData.acf.page_options.page_title.alignment === 'left',
+            'text-align__center' : wpData.acf.page_options.page_title.alignment === 'center',
+            'text-align__right' : wpData.acf.page_options.page_title.alignment === 'right'
+          }"
+          v-if="wpData.acf.page_options.page_title.visibility === true"
+          v-html="wpData.title.rendered"
+        />
       </div>
-      <h1
-        class="page__title"
-        :class="{
-          'text-align__left' : wpData.acf.page_options.page_title.alignment === 'left',
-          'text-align__center' : wpData.acf.page_options.page_title.alignment === 'center',
-          'text-align__right' : wpData.acf.page_options.page_title.alignment === 'right'
-        }"
-        v-if="wpData.acf.page_options.page_title.visibility === true"
-        v-html="wpData.title.rendered"
-      />
-    </div>
-    <div
-      class="page__content"
-    >
-      <Sections
-        v-if="wpData"
-        :data="wpData"
-      />
-      <p
-        v-else
+      <div
+        class="page__content"
       >
-        {{ $t('Not found') }}
-      </p>
-    </div>
+        <Sections
+          v-if="wpData"
+          :data="wpData"
+        />
+        <p
+          v-else
+        >
+          {{ $t('Not found') }}
+        </p>
+      </div>
+    </template>
+    <div v-else v-html="wpData.content.rendered"/>
   </div>
 </template>
 
--- pages/Post.vue
+++ pages/Post.vue
@@ -4,41 +4,44 @@
     class="post"
     :key="wpData.id"
   >
-    <div
-      class="post__heading margin__y--sm"
-    >
+      <template v-if="wpData.acf && wpData.acf.sections">
+        <div
+          class="post__heading margin__y--sm"
+        >
+          <div
+            class="post__breadcrumbs"
+          >
+          </div>
+          <h1
+            class="post__title"
+            v-html="wpData.title.rendered"
+          />
+        </div>
+      </template>
       <div
-        class="post__breadcrumbs"
+        class="post__content"
       >
+        <div class="author">Autor: </div>
+        <div class="date">Data: {{ wpData.date_gmt.replace('T', ' ') }}</div>
+        <div class="tags">Tagi: 
+          <span v-for="(tag, index) in wpData.tags.slice(0, -1)" :key="index">
+            {{ tag }},&nbsp;
+          </span>
+          <span v-if="wpData.tags.length > 0">
+            {{ wpData.tags[wpData.tags.length - 1] }}
+          </span>
+        </div>
+        <Sections
+          v-if="wpData && wpData.acf && wpData.acf.sections !== null"
+          :data="wpData"
+        />
+        <p
+          v-else
+        >
+          {{ $t('Not found') }}
+        </p>
       </div>
-      <h1
-        class="post__title"
-        v-html="wpData.title.rendered"
-      />
-    </div>
-    <div
-      class="post__content"
-    >
-      <div class="author">Autor: </div>
-      <div class="date">Data: {{ wpData.date_gmt.replace('T', ' ') }}</div>
-      <div class="tags">Tagi: 
-        <span v-for="(tag, index) in wpData.tags.slice(0, -1)" :key="index">
-          {{ tag }},&nbsp;
-        </span>
-        <span v-if="wpData.tags.length > 0">
-          {{ wpData.tags[wpData.tags.length - 1] }}
-        </span>
-      </div>
-      <Sections
-        v-if="wpData && wpData.acf && wpData.acf.sections !== null"
-        :data="wpData"
-      />
-      <p
-        v-else
-      >
-        {{ $t('Not found') }}
-      </p>
-    </div>
+    <div v-if="!(wpData.acf && wpData.acf.sections)" v-html="wpData.content.rendered"/>
   </div>
 </template>
 
