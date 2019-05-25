079e6ed20adf3c88873f8ba43956f5ff1143f74b
Aleksander Grygier <alek@grygier.co>
2019-05-25 18:55:21 +0200

    Quick fix for column (removed width--100 class for One Column)

--- components/Content/Column.vue
+++ components/Content/Column.vue
@@ -1,20 +1,17 @@
 <template>
   <div
     class="column flex"
-      >
-        <component
-          :is="column.cmpName"
-          v-if="column !== false"
-          :data="column"
-          :class="{
-            'width--100' : columnAmount === 1
-          }"
-        />
-        <div 
-          v-else
-          class='empty-column'
-        />
-      </div>
+  >
+    <component
+      :is="column.cmpName"
+      v-if="column !== false"
+      :data="column"
+    />
+    <div 
+      v-else
+      class='empty-column'
+    />
+  </div>
 </template>
 
 <script>
