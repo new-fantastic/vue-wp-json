1ed30edd1c2c46319846ddd09a00887dbd37674d
Fifciuu <filip.jdrasik@gmail.com>
2019-05-31 16:13:56 +0200

    Fetches data from multiple sources

--- mixins/loadSections.ts
+++ mixins/loadSections.ts
@@ -1,54 +1,187 @@
-import { ContentTypes, FetchHookTypes } from '../types'
+import { ContentTypes, FetchHookTypes, LoaderRequestElement } from '../types'
 import { ModulePrefix } from '../index'
+import { ContentTypeToString } from '../util/Filters'
 import Meta from '../plugin/inheritable/Meta'
 
+const LoaderRequestToFetches = 
+(slug: Array<LoaderRequestElement | string>, store)
+: Array<Promise<any>> => {
+  
+  const requests = []
+  
+  for (let requestElement of slug) {
+    if (typeof requestElement === 'string') {
+      
+      requests.push(
+        store.dispatch(`${ModulePrefix}_page/load`, {
+          slug: requestElement,
+          type: ContentTypes.Page
+        })
+      )
+
+    } else {
+      let slug = requestElement.slug
+      let dataName = slug
+      if('dataName' in requestElement) {
+        dataName = requestElement.dataName
+      }
+      let type = 'post' in requestElement && requestElement.post === true 
+        ? 'post'
+        : 'page'
+      
+      requests.push(
+        store.dispatch(`${ModulePrefix}_${type}/load`, {
+          slug,
+          type: type === 'post' 
+            ? ContentTypes.Post
+            : ContentTypes.Page
+        })
+      )
+
+    }
+  }
+
+  return requests
+}
+
 export default (
-  slug: string,
+  slug: string | Array<LoaderRequestElement | string>,
   createdOrAsync: FetchHookTypes = FetchHookTypes.Created) => {
 
   if(!slug || slug.length < 1) {
     throw new Error('Slug no provided')
   }
 
-  const mixin: any = {}
+  let mixin: any = {}
+
+  let computed: any = {}
 
   if (createdOrAsync === FetchHookTypes.Created) {
     // Appears in Vue version
-    mixin.computed = {
-      wpData () {
-        return this.$store.state[`${ModulePrefix}_page`].page[slug]
-          ? this.$store.state[`${ModulePrefix}_page`].page[slug]
+    computed = { }
+
+    if (typeof slug === 'string') {
+      computed[slug] = () => {
+        const type = ContentTypeToString(ContentTypes.Page)
+
+        return this.$store.state[`${ModulePrefix}_${type}`][type][slug]
+          ? this.$store.state[`${ModulePrefix}_${type}`][type][slug]
           : null
       }
+    } else {
+      // slug - array of request elements in this example
+      for (let requestElement of slug) {
+        if (typeof requestElement === 'string') {
+          computed = {
+            ...computed,
+            [requestElement]() {
+              const type = ContentTypeToString(ContentTypes.Page)
+
+              return this.$store.state[`${ModulePrefix}_${type}`][type][requestElement]
+                ? this.$store.state[`${ModulePrefix}_${type}`][type][requestElement]
+                : null
+            }
+          }
+        } else {
+          let slug = requestElement.slug
+          let dataName = slug
+          if('dataName' in requestElement) {
+            dataName = requestElement.dataName
+          }
+          let contentType = 'post' in requestElement && requestElement.post === true 
+            ? ContentTypes.Post
+            : ContentTypes.Page
+
+          computed = {
+            ...computed, 
+            [dataName]() { 
+              const type = ContentTypeToString(contentType)
+
+              return this.$store.state[`${ModulePrefix}_${type}`][type][slug]
+                ? this.$store.state[`${ModulePrefix}_${type}`][type][slug]
+                : null
+            }
+          }
+        }
+      }
+
+      mixin = { computed }
+
     }
 
-    mixin.created = async function () {
-  
-        await this.$store.dispatch(`${ModulePrefix}_page/load`, {
-          slug,
-          type: ContentTypes.Page
-        })
+    mixin = {
+      ...mixin, 
+      async created() {
 
+        const requests = []
+
+        if (typeof slug === 'string') {
+
+          requests.push(
+            this.$store.dispatch(`${ModulePrefix}_page/load`, {
+              slug,
+              type: ContentTypes.Page
+            })
+          )
+
+        } else {
+          // slug - array of request elements in this example
+          requests.push(...LoaderRequestToFetches(slug, this.$store))
+        }
+
+        await Promise.all(requests)
+      }
     }
   }
 
-  if (createdOrAsync === FetchHookTypes.AsyncData) {
+  else if (createdOrAsync === FetchHookTypes.AsyncData) {
     // Appears in Nuxt version
 
     mixin.asyncData = async function ({store}) {
-   
-      await store.dispatch(`${ModulePrefix}_page/load`, {
-        slug,
-        type: ContentTypes.Page
-      })
 
-      const wpData = store.state[`${ModulePrefix}_page`].page[slug]
-        ? store.state[`${ModulePrefix}_page`].page[slug]
-        : null
+      const requests = []
 
-      return {
-        wpData
+      if (typeof slug === 'string') {
+
+        requests.push(
+          store.dispatch(`${ModulePrefix}_page/load`, {
+            slug,
+            type: ContentTypes.Page
+          })
+        )
+
+      } else {
+        // slug - array of request elements in this example
+        requests.push(...LoaderRequestToFetches(slug, store))
       }
+
+      await Promise.all(requests)
+
+      const asyncData = {}
+
+      for (let requestElement of slug) {
+        if (typeof requestElement === 'string') {
+          asyncData[requestElement] = store.state[`${ModulePrefix}_page`].page[requestElement]
+            ? store.state[`${ModulePrefix}_page`].page[requestElement]
+            : null
+        }
+        else {
+          let slug = requestElement.slug
+          let dataName = slug
+          if('dataName' in requestElement) {
+            dataName = requestElement.dataName
+          }
+          let type = 'post' in requestElement && requestElement.post === true 
+            ? 'post'
+            : 'page'
+
+          asyncData[dataName] = store.state[`${ModulePrefix}_${type}`][type][slug]
+            ? store.state[`${ModulePrefix}_${type}`][type][slug]
+            : null
+        }
+      }
+
+      return asyncData
 
     }
 
