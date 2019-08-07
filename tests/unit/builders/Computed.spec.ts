da9dae2698d170ade44bf62f451bf3f65bfb1188
Fifciuu <filip.jdrasik@gmail.com>
2019-08-07 11:10:42 +0200

    Fixed tests for new features

--- tests/unit/builders/Computed.spec.ts
+++ tests/unit/builders/Computed.spec.ts
@@ -1,245 +1,224 @@
-import buildComputed from '../../../mixins/wpData/builders/Computed'
-import { ModulePrefix } from '../../../'
-import FetchHookTypeCreated from '../../../mixins/wpData/fetchHookTypes/Created'
-import { LoaderRequestElement, LoaderRequestElementWithValue } from '../../../types'
+import buildComputed from "../../../mixins/wpData/builders/Computed";
+import { ModulePrefix } from "../../../";
+import FetchHookTypeCreated from "../../../mixins/wpData/fetchHookTypes/Created";
+import {
+  LoaderRequestElement,
+  LoaderRequestElementWithValue
+} from "../../../types";
 
-describe('Computed Builder', () => {
-
-  it('computed returns proper value for loaderRequest: string', () => {
-
-    const slug = 'slug'
+describe("Computed Builder", () => {
+  it("computed returns proper value for loaderRequest: string", () => {
+    const slug = "slug";
     const that3 = {
       $store: {
         state: {
-          [`${ModulePrefix}_page`]: {
-            page: {
-              [slug]: 'secret_value'
+          [`${ModulePrefix}_post`]: {
+            types: {
+              pages: {
+                [slug]: "secret_value"
+              }
             }
           }
         }
       }
-    }
+    };
 
-    const value: any = FetchHookTypeCreated.call(that3, slug)
-    
-    expect(value.computed).toBeDefined()
-    expect(slug in value.computed).toBeTruthy()
-    expect(typeof value.computed.slug).toBe('function')
+    const value: any = FetchHookTypeCreated.call(that3, slug);
+
+    expect(value.computed).toBeDefined();
+    expect(slug in value.computed).toBeTruthy();
+    expect(typeof value.computed.slug).toBe("function");
     expect(value.computed.slug.call(that3)).toBe(
-      that3.$store.state[`${ModulePrefix}_page`].page[slug]
-    )
+      that3.$store.state[`${ModulePrefix}_post`].types.pages[slug]
+    );
+  });
 
-  })
-
-  it('computed returns null for loaderRequest: string if it is empty', () => {
-    const slug = 'slug'
+  it("computed returns null for loaderRequest: string if it is empty", () => {
+    const slug = "slug";
     const that3 = {
       $store: {
         state: {
-          [`${ModulePrefix}_page`]: {
-            page: {}
+          [`${ModulePrefix}_post`]: {
+            types: {
+              pages: {}
+            }
           }
         }
       }
-    }
+    };
 
-    const value: any = FetchHookTypeCreated.call(that3, slug)
-    
-    expect(value.computed).toBeDefined()
-    expect(slug in value.computed).toBeTruthy()
-    expect(typeof value.computed[slug]).toBe('function')
-    expect(value.computed.slug.call(that3)).toBeNull()
-  })
+    const value: any = FetchHookTypeCreated.call(that3, slug);
 
-  it('comptued returns proper value for loaderRequest: LoaderRequestElement', () => {
+    expect(value.computed).toBeDefined();
+    expect(slug in value.computed).toBeTruthy();
+    expect(typeof value.computed[slug]).toBe("function");
+    expect(value.computed.slug.call(that3)).toBeNull();
+  });
 
+  it("comptued returns proper value for loaderRequest: LoaderRequestElement", () => {
     const requests: LoaderRequestElementWithValue[] = [
       {
-        slug: 'slug1',
-        dataName: 'other1',
-        post: true,
-        value: 'abc'
+        slug: "slug1",
+        dataName: "other1",
+        type: "posts",
+        value: "abc"
       },
       {
-        slug: 'slug2',
-        value: 'abc'
+        slug: "slug2",
+        value: "abc"
       },
       {
-        slug: 'slug3',
-        dataName: 'other2',
-        value: 'abc'
+        slug: "slug3",
+        dataName: "other2",
+        value: "abc"
       },
       {
-        slug: 'slug4',
-        post: true,
-        value: 'abc'
+        slug: "slug4",
+        type: "posts",
+        value: "abc"
       }
-    ]
+    ];
     const that3 = {
       $store: {
         state: {
-          [`${ModulePrefix}_page`]: {
-            page: { }
-          },
           [`${ModulePrefix}_post`]: {
-            post: { }
+            types: {
+              posts: {},
+              pages: {}
+            }
           }
         }
       }
-    }
+    };
 
     for (let request of requests) {
+      const dataName = "dataName" in request ? request.dataName : request.slug;
 
-      const dataName = 'dataName' in request
-        ? request.dataName
-        : request.slug
+      const contentType =
+        "type" in request && request.type ? request.type : "pages";
 
-      const contentType = 'post' in request && request.post
-        ? 'post'
-        : 'page'
+      that3.$store.state[`${ModulePrefix}_post`].types[contentType][
+        request.slug
+      ] = request.value;
 
-      that3.$store.state[`${ModulePrefix}_${contentType}`]
-      [contentType]
-      [request.slug] = request.value
+      const value: any = FetchHookTypeCreated.call(that3, request);
 
-      const value: any = FetchHookTypeCreated.call(that3, request)
-  
-      expect(value.computed).toBeDefined()
-      expect(dataName in value.computed).toBeTruthy()
-      expect(typeof value.computed[dataName]).toBe('function')
+      expect(value.computed).toBeDefined();
+      expect(dataName in value.computed).toBeTruthy();
+      expect(typeof value.computed[dataName]).toBe("function");
 
       expect(value.computed[dataName].call(that3)).toBe(
-        that3.$store.state[`${ModulePrefix}_${contentType}`]
-        [contentType]
-        [request.slug]
-      )
-
+        that3.$store.state[`${ModulePrefix}_post`].types[contentType][
+          request.slug
+        ]
+      );
     }
+  });
 
-  })
-
-  it('computed returns proper value for loaderRequest: Array<LoaderRequestElement | string>', () => {
-
+  it("computed returns proper value for loaderRequest: Array<LoaderRequestElement | string>", () => {
     const requestsToSend: Array<LoaderRequestElement | string> = [
       {
-        slug: 'slug1',
-        dataName: 'other1',
-        post: true
+        slug: "slug1",
+        dataName: "other1",
+        type: "posts"
       },
       {
-        slug: 'slug2'
+        slug: "slug2"
       },
       {
-        slug: 'slug3',
-        dataName: 'other2'
+        slug: "slug3",
+        dataName: "other2"
       },
       {
-        slug: 'slug4',
-        post: true
+        slug: "slug4",
+        type: "posts"
       },
-      'hello'
-    ]
+      "hello"
+    ];
 
     const requestsLocal: Array<LoaderRequestElementWithValue | string> = [
       {
-        slug: 'slug1',
-        dataName: 'other1',
-        post: true,
-        value: 'abc'
+        slug: "slug1",
+        dataName: "other1",
+        type: "posts",
+        value: "abc"
       },
       {
-        slug: 'slug2',
-        value: 'abc'
+        slug: "slug2",
+        value: "abc"
       },
       {
-        slug: 'slug3',
-        dataName: 'other2',
-        value: 'abc'
+        slug: "slug3",
+        dataName: "other2",
+        value: "abc"
       },
       {
-        slug: 'slug4',
-        post: true,
-        value: 'abc'
+        slug: "slug4",
+        type: "posts",
+        value: "abc"
       },
-      'hello'
-    ]
+      "hello"
+    ];
 
-    const stringsValue: string = 'abc'
+    const stringsValue: string = "abc";
 
     const that3 = {
       $store: {
         state: {
-          [`${ModulePrefix}_page`]: {
-            page: { }
-          },
           [`${ModulePrefix}_post`]: {
-            post: { }
+            types: {
+              posts: {},
+              pages: {}
+            }
           }
         }
       }
-    }
+    };
 
     for (let request of requestsLocal) {
-
-      if(typeof request === 'string') {
-
-        that3.$store.state[`${ModulePrefix}_page`].page
-        [request] = stringsValue
-
+      if (typeof request === "string") {
+        that3.$store.state[`${ModulePrefix}_post`].types.pages[
+          request
+        ] = stringsValue;
       } else {
+        const contentType =
+          "type" in request && request.type ? request.type : "pages";
 
-        const contentType = 'post' in request && request.post
-          ? 'post'
-          : 'page'
-
-        that3.$store.state[`${ModulePrefix}_${contentType}`]
-        [contentType]
-        [request.slug] = request.value
-
+        that3.$store.state[`${ModulePrefix}_post`].types[contentType][
+          request.slug
+        ] = request.value;
       }
-
     }
 
-    const value: any = FetchHookTypeCreated.call(that3, requestsToSend)
+    const value: any = FetchHookTypeCreated.call(that3, requestsToSend);
 
-    expect(value.computed).toBeDefined()
-    expect(Object.keys(value.computed).length).toBe(requestsToSend.length)
+    expect(value.computed).toBeDefined();
+    expect(Object.keys(value.computed).length).toBe(requestsToSend.length);
 
-    for(let request of requestsLocal) {
-
-      if (typeof request === 'string') {
-
-        expect(request in value.computed).toBeTruthy()
-        expect(typeof value.computed[request]).toBe('function')
+    for (let request of requestsLocal) {
+      if (typeof request === "string") {
+        expect(request in value.computed).toBeTruthy();
+        expect(typeof value.computed[request]).toBe("function");
 
         expect(value.computed[request].call(that3)).toBe(
-          that3.$store.state[`${ModulePrefix}_page`].page
-          [request]
-        )
-
+          that3.$store.state[`${ModulePrefix}_post`].types.pages[request]
+        );
       } else {
+        const dataName =
+          "dataName" in request ? request.dataName : request.slug;
 
-        const dataName = 'dataName' in request
-          ? request.dataName
-          : request.slug
+        const contentType =
+          "type" in request && request.type ? request.type : "pages";
 
-        const contentType = 'post' in request && request.post
-          ? 'post'
-          : 'page'
-
-        expect(dataName in value.computed).toBeTruthy()
-        expect(typeof value.computed[dataName]).toBe('function')
+        expect(dataName in value.computed).toBeTruthy();
+        expect(typeof value.computed[dataName]).toBe("function");
 
         expect(value.computed[dataName].call(that3)).toBe(
-          that3.$store.state[`${ModulePrefix}_${contentType}`]
-          [contentType]
-          [request.slug]
-        )
-
+          that3.$store.state[`${ModulePrefix}_post`].types[contentType][
+            request.slug
+          ]
+        );
       }
-
     }
-
-  })
-
-})
+  });
+});
--- tests/unit/fht/Created.spec.ts
+++ tests/unit/fht/Created.spec.ts
@@ -1,140 +1,110 @@
-import FetchHookTypeCreated from '../../../mixins/wpData/fetchHookTypes/Created'
-import pickMetaSource from '../../../mixins/PickMetaSource'
-import { ModulePrefix } from '../../../'
-import { ContentTypes, LoaderRequestElement } from '../../../types';
+import FetchHookTypeCreated from "../../../mixins/wpData/fetchHookTypes/Created";
+import pickMetaSource from "../../../mixins/PickMetaSource";
+import { ModulePrefix } from "../../../";
+import { ContentTypes, LoaderRequestElement } from "../../../types";
 
-describe('FetchHookType: Created', () => {
-
+describe("FetchHookType: Created", () => {
   const that = {
     $store: {
-      dispatch (state, payload) {
+      dispatch(state, payload) {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
-            resolve()
-          }, 100)
-        })
+            resolve();
+          }, 100);
+        });
       }
     }
-  }
+  };
 
-  
+  it("builds proper Created for loaderRequest: string", () => {
+    const slug = "slug";
 
-  it('builds proper Created for loaderRequest: string', () => {
+    const value: any = FetchHookTypeCreated.call(that, slug);
+    const spy = jest.spyOn(that.$store, "dispatch");
+    const invoke = value.created.call(that);
 
-    const slug = 'slug'
+    expect(spy).toHaveBeenCalledWith(`${ModulePrefix}_post/load`, {
+      slug: slug,
+      type: "pages"
+    });
+  });
 
-    const value: any = FetchHookTypeCreated.call(that, slug)
-    const spy = jest.spyOn(that.$store, 'dispatch')
-    const invoke = value.created.call(that)
-
-    expect(spy).toHaveBeenCalledWith(
-      `${ModulePrefix}_page/load`,
-      {
-        slug: slug,
-        type: ContentTypes.Page
-      }
-    )
-
-  })
-
-  it('builds proper Created for loaderRequest: LoaderRequest', () => {
-
+  it("builds proper Created for loaderRequest: LoaderRequest", () => {
     const tests: LoaderRequestElement[] = [
       {
-        slug: 'slug',
-        post: true
+        slug: "slug",
+        type: "posts"
       },
       {
-        slug: 'slug2',
-        post: false
+        slug: "slug2",
+        type: "pages"
       },
       {
-        slug: 'slug3'
+        slug: "slug3"
       }
-    ]
+    ];
 
     for (let test of tests) {
+      const value: any = FetchHookTypeCreated.call(that, test);
+      const spy = jest.spyOn(that.$store, "dispatch");
+      value.created.call(that);
 
-      const value: any = FetchHookTypeCreated.call(that, test)
-      const spy = jest.spyOn(that.$store, 'dispatch')
-      const invoke = value.created.call(that)
-
-      expect(spy).toHaveBeenCalledWith(
-        'post' in test && test.post 
-          ? `${ModulePrefix}_post/load`
-          : `${ModulePrefix}_page/load`,
-        {
-          slug: test.slug,
-          type: 'post' in test && test.post 
-            ? ContentTypes.Post 
-            : ContentTypes.Page
-        }
-      )
-
+      expect(spy).toHaveBeenCalledWith(`${ModulePrefix}_post/load`, {
+        slug: test.slug,
+        type: "type" in test && test.type ? test.type : "pages"
+      });
     }
+  });
 
-  })
-
-  it('builds proper Created for loaderRequest: Array<LoaderRequest | string>', async () => {
-
+  it("builds proper Created for loaderRequest: Array<LoaderRequest | string>", async () => {
     const test = [
       {
-        slug: 'slug',
-        post: true
+        slug: "slug",
+        type: "posts"
       },
       {
-        slug: 'slug2',
-        post: false
+        slug: "slug2",
+        type: "pages"
       },
-      'slug3'
-    ]
+      "slug3"
+    ];
 
     const that2 = {
       $store: {
-        dispatch (state, payload) {
+        dispatch(state, payload) {
           return new Promise((resolve, reject) => {
             setTimeout(() => {
-              resolve()
-            }, 100)
-          })
+              resolve();
+            }, 100);
+          });
         }
       }
-    }
+    };
 
-    const value: any = FetchHookTypeCreated.call(that2, test)
-    const spy = jest.spyOn(that2.$store, 'dispatch')
-    const invoke = await value.created.call(that2)
+    const value: any = FetchHookTypeCreated.call(that2, test);
+    const spy = jest.spyOn(that2.$store, "dispatch");
+    await value.created.call(that2);
 
     for (let i = 0, len = test.length; i < len; i++) {
-
-      expect(spy).toHaveBeenNthCalledWith(i+1,
-        typeof test[i] !== 'string' && 'post' in test[i] && test[i].post 
-          ? `${ModulePrefix}_post/load`
-          : `${ModulePrefix}_page/load`,
-        {
-          slug: typeof test[i] === 'string' ? test[i] : test[i].slug,
-          type: typeof test[i] !== 'string' && 'post' in test[i] && test[i].post
-            ? ContentTypes.Post 
-            : ContentTypes.Page
-        }
-      )
-
+      expect(spy).toHaveBeenNthCalledWith(i + 1, `${ModulePrefix}_post/load`, {
+        slug: typeof test[i] === "string" ? test[i] : test[i].slug,
+        type:
+          typeof test[i] !== "string" && "type" in test[i]
+            ? test[i].type
+            : "pages"
+      });
     }
+  });
 
-  })
-
-  it('throws error when buildCreated gets a bad argument', async () => {
-
-    let error
+  it("throws error when buildCreated gets a bad argument", async () => {
+    let error;
     try {
-      const value: any = FetchHookTypeCreated.call(that, [155,12]) // Bad arg
-      await value.created.call(that)
-    } catch(e) {
-      error = e
+      const value: any = FetchHookTypeCreated.call(that, [155, 12]); // Bad arg
+      await value.created.call(that);
+    } catch (e) {
+      error = e;
     }
 
-    expect(error).toBeInstanceOf(Error)
-
-  })
-
-})
+    expect(error).toBeInstanceOf(Error);
+  });
+});
