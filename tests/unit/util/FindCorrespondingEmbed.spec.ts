d36f4729e7944f297004c06c3b19527575db4291
Fifciuu <filip@newfantastic.com>
2020-02-09 23:11:06 +0100

    Finding categories

--- tests/unit/util/FindCorrespondingEmbed.spec.ts
+++ tests/unit/util/FindCorrespondingEmbed.spec.ts
@@ -2,7 +2,7 @@ import FindCorrespondingEmbed from '../../../util/FindCorrespondingEmbed'
 
 describe('FindCorrespondingEmbed', () => {
 
-  it('finds "author" properly', () => {
+  it('finds "author" or a few ones properly', () => {
     const post = {
       _embedded: {
         author: [
@@ -22,7 +22,7 @@ describe('FindCorrespondingEmbed', () => {
       }
     }
 
-    const PostWithAuthorId = (author: Number) => ({
+    const PostWithAuthorId = (author: Number | Array<Number>) => ({
       ...post,
       author
     })
@@ -53,6 +53,166 @@ describe('FindCorrespondingEmbed', () => {
         )
       )
     ).toBe(JSON.stringify(post._embedded.author[2]))
+
+    expect(
+      JSON.stringify(
+        FindCorrespondingEmbed(
+          PostWithAuthorId([3, 4]),
+          'author'
+        )
+      )
+    ).toBe(JSON.stringify([
+      post._embedded.author[2],
+      post._embedded.author[1]
+    ]))
+  })
+
+  it('finds "featured_media" or a few ones properly', () => {
+    const post = {
+      _embedded: {
+        'wp:featuredmedia': [
+          {
+            id: 5,
+            name: 'fifciuu'
+          },
+          {
+            id: 4,
+            name: 'undefined'
+          },
+          {
+            id: 3,
+            name: 'defined'
+          }
+        ]
+      }
+    }
+
+    const PostWithFeaturedImageId = (featured_media: Number | Array<Number>) => ({
+      ...post,
+      featured_media
+    })
+
+    expect(
+      JSON.stringify(
+        FindCorrespondingEmbed(
+          PostWithFeaturedImageId(5),
+          'featured_media'
+        )
+      )
+    ).toBe(JSON.stringify(post._embedded[`wp:featuredmedia`][0]))
+
+    expect(
+      JSON.stringify(
+        FindCorrespondingEmbed(
+          PostWithFeaturedImageId(4),
+          'featured_media'
+        )
+      )
+    ).toBe(JSON.stringify(post._embedded[`wp:featuredmedia`][1]))
+
+    expect(
+      JSON.stringify(
+        FindCorrespondingEmbed(
+          PostWithFeaturedImageId(3),
+          'featured_media'
+        )
+      )
+    ).toBe(JSON.stringify(post._embedded[`wp:featuredmedia`][2]))
+
+    expect(
+      JSON.stringify(
+        FindCorrespondingEmbed(
+          PostWithFeaturedImageId([3, 4]),
+          'featured_media'
+        )
+      )
+    ).toBe(JSON.stringify([
+      post._embedded[`wp:featuredmedia`][2],
+      post._embedded[`wp:featuredmedia`][1]
+    ]))
+  })
+
+  it('finds "category" or a few ones properly', () => {
+    const post = {
+      _embedded: {
+        'wp:term': [
+          [
+            {
+              id: 5,
+              name: 'fifciuu',
+              taxonomy: 'category'
+            },
+            {
+              id: 4,
+              name: 'undefined',
+              taxonomy: 'category'
+            },
+            {
+              id: 3,
+              name: 'defined',
+              taxonomy: 'category'
+            }
+          ],
+          [
+            {
+              id: 9,
+              name: 'fifciuu',
+              taxonomy: 'category'
+            },
+            {
+              id: 10,
+              name: 'undefined',
+              taxonomy: 'category'
+            },
+            {
+              id: 11,
+              name: 'defined',
+              taxonomy: 'category'
+            }
+          ]
+        ]
+      }
+    }
+
+    const PostWithCategoryId = (category: Number | Array<Number>) => ({
+      ...post,
+      category
+    })
+
+    expect(
+      JSON.stringify(
+        FindCorrespondingEmbed(
+          PostWithCategoryId(5),
+          'category'
+        )
+      )
+    ).toBe(JSON.stringify([post._embedded[`wp:term`][0][0]]))
+
+    expect(
+      JSON.stringify(
+        FindCorrespondingEmbed(
+          PostWithCategoryId(4),
+          'category'
+        )
+      )
+    ).toBe(JSON.stringify([post._embedded[`wp:term`][0][1]]))
+
+    expect(
+      JSON.stringify(
+        FindCorrespondingEmbed(
+          PostWithCategoryId(3),
+          'category'
+        )
+      )
+    ).toBe(JSON.stringify([post._embedded[`wp:term`][0][2]]))
+
+    expect(FindCorrespondingEmbed(
+      PostWithCategoryId([4, 3]),
+      'category'
+    ).sort((a, b) => a.id - b.id)).toMatchObject([
+      post._embedded[`wp:term`][0][2],
+      post._embedded[`wp:term`][0][1]
+    ].sort((a, b) => a.id - b.id))
   })
 
 })
--- util/FindCorrespondingEmbed.ts
+++ util/FindCorrespondingEmbed.ts
@@ -11,8 +11,47 @@ export default (response: any, key: string): any => {
       return identifier.map(identifier => response._embedded[key].find(record => record.id === identifier))
         .filter(v => !!v)
     }
-  } else {
-    // try to find wp:
+  } 
+
+  const featuredMedia = response._embedded[`wp:${key.split('_').join('')}`]
+  
+  if (featuredMedia) {
+    if (!Array.isArray(identifier)) {
+      return featuredMedia.find(record => record.id === identifier)
+    } else {
+      return identifier.map(identifier => featuredMedia.find(record => record.id === identifier))
+        .filter(v => !!v)
+    }
   }
-  return response
+
+  // Try to find in wp:term
+  const wpTerms = response._embedded['wp:term']
+  if (wpTerms) {
+    const matchingResults = []
+    const containsIds = (id: Number): Boolean => {
+      if (Array.isArray(identifier)) {
+        return identifier.some(identifier => identifier === id)
+      } else {
+        return identifier === id
+      }
+    }
+
+    for (let wpTerm of wpTerms) {
+
+      const localMatches = wpTerm.filter(term =>
+        term
+        && term.taxonomy === key
+        && containsIds(term.id)
+      )
+
+      if (localMatches && !!localMatches.length) {
+        matchingResults.push(...localMatches)
+      }
+
+    }
+
+    return matchingResults
+  }
+  
+  throw new Error('Could not find corresponding field')
 }
